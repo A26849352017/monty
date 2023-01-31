4c93b93093a267ae36d8e57cd075846abac3cac1
Trevor <chelimotrevorkibowen@gmail.com>
2023-01-31 12:37:55 +0300

    monty commit

--- queue.c
+++ queue.c
@@ -1,15 +1,51 @@
 #include "monty.h"
 
 /**
- * _queue - select stack mode (default).
- * @stack: double pointer to header (top) of the stack.
- * @line_number: counter for line number of the file.
- *
- * Return: void.
- */
-void _queue(stack_t **stack, unsigned int line_number)
+* f_queue - function that prints the top of the queue
+* @head: head of queue
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_queue(stack_t **head, unsigned int counter)
 {
-	(void) stack;
-	(void) line_number;
-	stack_queue = 'q';
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+* addqueue - function that add node to the tail the queue
+* @n: new value
+* @head: head of the queue
+*
+* Return: nothing
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (temp)
+	{
+		while (temp->next)
+			temp = temp->next;
+	}
+	if (!temp)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
 }
